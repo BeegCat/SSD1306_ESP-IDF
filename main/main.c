72fe9844672f265394162abcd38b2448cd80872b
BeegCat <bhyresh03@gmail.com>
2026-04-14 14:49:46 +0530

    initialized the I2C configuration struct

--- main/main.c
+++ main/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include "ssd1306.h"
 
 void app_main(void)
 {
+
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = SDA,
+        .scl_io_num = SCL,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_FREQUENCY
+    };
+
+    i2c_param_config(I2C_NUM_0, &i2c_config);
+    i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
 
 }
